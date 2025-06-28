d54a61d66bfbb659ae9c4f170d46216762a9bb82
Tanveer Ahmad <119504718+TanveerAhamd@users.noreply.github.com>
2025-06-28 18:17:50 +0500

    dummy cv uploaded

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const Links = {
     github: "https://github.com/TanveerAhamd",
     linkedin: "https://www.linkedin.com/in/tanveer-ahmad-718a5014b/",
     email: "mailto:mtanveerulhassan2@gmail.com",
-    resume: "public/imgs/Tanveer_Ahmad_Dev_CV.pdf",
+    resume: "https://drive.google.com/file/d/1ocjvkiQHV-HOUjUwzfSGLxGGFRUEdYCf/view?usp=sharing",
     emailonly: "mtanveerulhassan2@gmail.com",
     location: "Lodhran, Pakistan",
     phone: "+92 3000 695646",
