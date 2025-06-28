604df4ca4a15f2a9be84780d6c93cc4422c56a6d
Tanveer Ahmad <119504718+TanveerAhamd@users.noreply.github.com>
2025-06-28 17:52:11 +0500

    last done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -140,7 +140,7 @@ const Navbar = () => {
               </a>
             ))}
             <Button size="sm" className="ml-3 mt-2" asChild>
-              <a href={resumeUrl} download="Aathif_Zahir_CV.pdf">
+              <a href={resumeUrl} download="Tanveer_Ahmad_Dev_CV.pdf">
                 Resume
               </a>
             </Button>
